120333fc51072bf2bd60510337c2b5ac7ef85011
jmdelvalle <jeremy.delvalle@flatironschool.com>
2015-11-18 16:03:49 -0500

    nice intro to models in backbone

--- public/js/models/user.js
+++ public/js/models/user.js
@@ -1,1 +1,18 @@
 //  Extend the Backbone.Model prototype to build your own model here
+var User = Backbone.Model.extend({
+  defaults: function (){
+    return {
+      fullname: '',
+      isAdmin: false,
+      birthday: 1988
+    };
+  }
+});
+
+User.prototype.calculateAge = function () {
+  this.attributes.age = new Date().getFullYear() - this.attributes.birthday;
+};
+
+User.prototype.makeAdmin = function () {
+  this.attributes.isAdmin = true;
+};
