fc147970bf7f2196a00d591e4590b2ce60953958
dylan.okeefe <dylanokeefe@gmail.com>
2015-11-18 16:50:26 -0500

    modeled

--- public/js/models/user.js
+++ public/js/models/user.js
@@ -1,1 +1,21 @@
+var User = Backbone.Model.extend({
+  defaults : function(){
+    return {
+      fullname: " ",
+      firstName: " ",
+      lastName: " ",
+      isAdmin: false,
+      birthday: 1988
+    }
+  },
+  calculateAge : function(){
+    var age = (2015 - this.get('birthday'));
+    this.set({age: age})
+  },
+  makeAdmin : function(){
+    this.set({isAdmin: true})
+  }
+
+});
+
 //  Extend the Backbone.Model prototype to build your own model here
