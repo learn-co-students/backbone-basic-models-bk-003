788a0b2a00df3174354c85d4f85920f56f299cc5
Lawrence Chong <mail.to.lawrence@gmail.com>
2015-11-18 16:28:15 -0500

    fin

--- public/js/models/user.js
+++ public/js/models/user.js
@@ -1,1 +1,21 @@
 //  Extend the Backbone.Model prototype to build your own model here
+
+var User = Backbone.Model.extend({
+  defaults: function (){
+    return {
+      fullname: "J.K Rowling",
+      firstName: 'J.K', 
+      lastName: 'Rowling', 
+      isAdmin: false,
+      birthday: 1988,
+      age: 0
+    };
+  },
+  calculateAge: function(){
+  	this.set({'age': new Date().getFullYear() - this.get('birthday')});
+  	// return this.attributes.lastName; //this.get(birthday);
+  },
+  makeAdmin: function () {
+  	this.set({'isAdmin': true});
+  }
+})
