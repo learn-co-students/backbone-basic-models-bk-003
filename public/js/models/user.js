e5c6473bd8e44f6757e7c68abcc7adfb38c81d30
Alexander Burton <alexanderjburton@gmail.com>
2015-11-19 09:09:10 -0500

    toAdmin not isAdmin

--- public/js/models/user.js
+++ public/js/models/user.js
@@ -1,1 +1,15 @@
 //  Extend the Backbone.Model prototype to build your own model here
+var User = Backbone.Model.extend({
+  defaults : function (){
+    return {
+      fullname: " "
+    };
+  },
+  calculateAge : function (){
+  	var age = (2015 - this.get('birthday'));
+  	this.set("age", age);
+  },
+  makeAdmin : function (){
+  	this.set("isAdmin", true);
+  }
+});
