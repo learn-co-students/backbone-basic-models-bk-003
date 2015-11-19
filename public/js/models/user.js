adc37e4dd4137558668e82d32ca6df82cf1b7706
github username <mendelblesofsky@gmail.com>
2015-11-19 10:26:00 -0500

    backbrace

--- public/js/models/user.js
+++ public/js/models/user.js
@@ -1,1 +1,20 @@
 //  Extend the Backbone.Model prototype to build your own model here
+var User = Backbone.Model.extend({
+	defaults: function(){
+		return {
+			"fullname": '',
+			"isAdmin": false,
+			"birthday": 1988,
+			"age": 0
+		};
+	},
+	calculateAge: function(){
+		var date = new Date().getFullYear();
+		this.set({'age': date - this.get('birthday')});
+		return this.get('age')
+	},
+
+	makeAdmin: function(){
+		this.set({'isAdmin': true});
+	}	
+});
