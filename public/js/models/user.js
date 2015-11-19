b4244e360d51cab08bc2dc17f3bce48c84a6304d
Umer <myemailum14@gmail.com>
2015-11-19 10:45:55 -0500

    done

--- public/js/models/user.js
+++ public/js/models/user.js
@@ -1,1 +1,16 @@
 //  Extend the Backbone.Model prototype to build your own model here
+var User = Backbone.Model.extend({
+	  defaults: {
+	    "firstName":  "caesar salad",
+	    "lastName":     "ravioli",
+	    "fullname": "lololol blblblbl",
+	    "birthday":   1988,
+	    "isAdmin":    false
+	  },
+	  calculateAge: function () {
+	  	this.set('age',new Date().getFullYear() - this.get('birthday'));
+	  },
+	  makeAdmin : function() {
+	  	this.set("isAdmin",true);
+	  }
+});
