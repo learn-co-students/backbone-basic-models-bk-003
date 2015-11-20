bce8bc0227899d56b30ddb0711098d520101bfde
djoseph1250 <djoseph1250@gmail.com>
2015-11-19 19:10:47 -0500

    done

--- public/js/models/user.js
+++ public/js/models/user.js
@@ -1,1 +1,27 @@
 //  Extend the Backbone.Model prototype to build your own model here
+var User = Backbone.Model.extend({
+	defaults : function() {
+		return {
+			fullname: ' ',
+			firstName: ' ',
+			lastName: ' ',
+			isAdmin: false,
+			birthday: 1988,
+			age: null
+		};
+	},
+
+	calculateAge : function() {
+		var date = new Date();
+		var year = date.getFullYear();
+		var bday = this.get('birthday');
+		var age = (year - bday);
+		this.set('age', age);
+		return age;
+	},
+
+	makeAdmin : function() {
+		this.set('isAdmin', true);
+	},
+});
+
