94f45659e4d75c798f5e27b16fefe50df0565898
Giancarlo Martinez <g.r.rmartinez49@gmail.com>
2015-11-23 10:09:45 -0500

    create user model and add functions

--- public/js/models/user.js
+++ public/js/models/user.js
@@ -1,1 +1,23 @@
 //  Extend the Backbone.Model prototype to build your own model here
+
+var User = Backbone.Model.extend({
+  defaults: function (){
+    return {
+      fullname: "Giancarlo Martinez",
+	  firstName: "Giancarlo",
+	  lastName: "Martinez",
+	  isAdmin: false,
+	  birthday: 1991
+	};
+  },
+  calculateAge: function (){
+  	var today = (new Date).getFullYear()
+	this.set({'age': today - this.get('birthday')});
+	return this.get('age')
+  },
+  makeAdmin: function(){
+  	this.set({'isAdmin': true})
+  }
+});
+
+
