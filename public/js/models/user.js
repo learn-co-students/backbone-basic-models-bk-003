cf000d2511287402425f0571bd25741a66913394
sdolmo <sylviad1221@gmail.com>
2015-11-30 14:48:22 -0500

    done

--- public/js/models/user.js
+++ public/js/models/user.js
@@ -1,1 +1,15 @@
-//  Extend the Backbone.Model prototype to build your own model here
+var User = Backbone.Model.extend({
+  defaults: {
+    fullname: " ",
+    isAdmin: false,
+    birthday: 1988,
+  },
+  calculateAge: function(){
+    var date = new Date();
+    var age = date.getFullYear() - this.get('birthday');
+    this.set("age", age)
+  },
+  makeAdmin: function(){
+    this.set("isAdmin", true)
+  }
+});
