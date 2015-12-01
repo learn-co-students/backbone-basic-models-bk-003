52cb0dfb9d26d48fc6595a190017c01378548458
kinglemuel <pro@kinglemuel.com>
2015-12-01 15:34:56 -0500

    pass

--- public/js/models/user.js
+++ public/js/models/user.js
@@ -1,1 +1,20 @@
 //  Extend the Backbone.Model prototype to build your own model here
+
+var User = Backbone.Model.extend({
+	defaults: {
+		firstName: "Lemuel",
+		lastName: "Uhuru",
+		fullname: "Lemuel Uhuru",
+		isAdmin: false,
+		birthday: 1988,
+		age: 0
+	},
+
+	calculateAge: function() {
+		this.set({age: (new Date().getFullYear() - this.get('birthday'))});
+	},
+
+	makeAdmin: function() {
+		this.set({ isAdmin: true });
+	}
+});
